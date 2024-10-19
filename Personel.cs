3da3a2928c5c5e604edbf4bbbd4b509da365ec0a
emrahelis <emrahelis@gmail.com>
2024-10-19 12:53:10 +0300

    sdsd

--- Personel.cs
+++ Personel.cs
@@ -3,11 +3,20 @@ using System.Runtime;
 public class Personel
 {
 
-
+    public int id;
     // Yeni nesil prop yazarken, artık değişkenleri yazmaya gerek yok 
 
+    // Otomatik Property'ler : Bu property çeşitleri genelde hiç bir private field'i kapsüllemezler
+    // kapsülleme ihtiyacınız olduğunda bir önceki yöntem ile kapsülleme işlemi yapılabilir
+
+    // 
     // Property
-    public int Id { get; set; }
+    public int Id
+    {
+        get { return id; }
+        set
+        { id = value; }
+    }
     public string Ad { get; set; }
 
     // yeni yöntemde, kapsüllemede sınıfa ait değişkenler yoktur.(Field) Direk olarak Property'ler yazılır
@@ -19,7 +28,42 @@ public class Personel
     public int Yas { get; private set; }
 
     // istenilirse, bloklardan herhangi birini yazmayabilirsiniz!!!
-    public double Maas { get;}
+    public double Maas { get; }
 
-    // 
+    // istenilirse, pğroperty'lere başlangıç değeri verilebilir
+    public string IsYeri { get; set; } = "Karabük demir çelik tesisleri";
+
+    public Personel(int id, string ad, int yas, string isyeri)
+    {
+        // ortamlarda, eğer otomatik prop varsa ctor içerisinden gelen değerler
+        // direk olarak otomatik proplara maplenirler, zaten otomatik prop kullanıyorsa, 
+        //field kullanmıyoruzdur!!
+
+        this.Id = id;
+        this.Ad = ad;
+        this.Yas = yas;
+        this.IsYeri = isyeri;
+    }
+
 }
+
+// Ctor ile prop kullanımı nasıl olacak !!! 
+
+
+// Bu konuyu, property ve otomatik property kavramlarını salı günü tekrar konuşacağız.!!!
+
+
+// Yeni kavramlar nelerdir : 
+
+// Bir class'ın üyesi olan değişkene Field denir
+// bir metodun üyesi değişkene local değişken denir
+// bir sınıfın encapsulation üyesine property denir!!
+
+// not : iki türlü değişken vardır, birisi metotların ilerisinde yaşayan ve ölen değişkenlerdir.
+// bu değişkenlere local değişken denir
+// diğer bir değişken türüde, sınıf üyesi değişkenlerdir. Bu değişkenler sınıf yaşadığı sürece yaşarlar
+// metotlardan bu değişkenlere erişilebilir
+// mantık olarak ,üye seviyesi olarak meetotlarla aynı seviyedelerdir
+// sınıf üyesi değişkenlerede field denir 
+
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Personel p  = new Personel();
+Console.WriteLine(p.IsYeri);
+
+
+// Otomatik Propery'lere değer atasak ? 
+// iş yeri alanını değiştirelim 
+
+// otomatik prop üzerine değer atama!!!!!
+p.IsYeri="İskenderun Demir Çelik Fabrikası";
+Console.WriteLine(p.IsYeri);
